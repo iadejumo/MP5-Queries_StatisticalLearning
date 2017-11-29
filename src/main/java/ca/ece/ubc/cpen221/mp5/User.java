8904b02370c5e6ac445bce51a0eaaffd83cc0d55
iadejumo <isaiahadejumo@gmail.com>
2017-11-28 22:35:25 -0800

    Construtors for Resaurant, Review & User

--- src/main/java/ca/ece/ubc/cpen221/mp5/User.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/User.java
@@ -2,15 +2,47 @@ package ca.ece.ubc.cpen221.mp5;
 
 import java.util.Map;
 
+import org.json.simple.JSONObject;
+
 public class User {
 
 	private String url;
 	private Map<String,Integer> votes;
-	private int review_count;
+	private long review_count;
 	private String type;
 	private String user_id;
 	private String name;
-	private String average_stars;
+	private Double average_stars;
 	
+	public User(JSONObject user) {
+		this.url = (String) user.get("url");
+		this.votes = (Map<String,Integer>) user.get("votes");
+		this.review_count = (long) user.get("review_count");
+		this.type = (String) user.get("type");
+		this.user_id = (String) user.get("user_id");
+		this.name = (String) user.get("name");
+		this.average_stars = (Double) user.get("average_stars");
+	}
 	
+	public String getUrl() {
+		return url;
+	}
+	public Map<String,Integer> getVotes() {
+		return votes;
+	}
+	public long getReview_count() {
+		return review_count;
+	}
+	public String getType() {
+		return type;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Double getAverage_stars() {
+		return average_stars;
+	}
 }
--- src/main/java/ca/ece/ubc/cpen221/mp5/YelpDB.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/YelpDB.java
@@ -1,6 +1,8 @@
 package ca.ece.ubc.cpen221.mp5;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.ToDoubleBiFunction;
 
@@ -10,6 +12,10 @@ public class YelpDB implements MP5Db<Object> {
 	private File restaurants;
 	private File reviews;
 	private File users;
+	
+	private Map<String, Restaurant> restaurantMap = new HashMap<String, Restaurant> ();
+	private Map<String, Review> reviewMap = new HashMap<String, Review> ();
+	private Map<String, User> userMap = new HashMap<String, User> ();
 	
 	public YelpDB(String restaurantFilename,String reviewFilename, String userFilename) {
 		
