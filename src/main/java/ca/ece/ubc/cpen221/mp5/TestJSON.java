52350b197c433592c5572e97c86b43b4034eb302
iadejumo <isaiahadejumo@gmail.com>
2017-11-28 20:45:29 -0800

    Fixed JSON parser test

--- src/main/java/ca/ece/ubc/cpen221/mp5/TestJSON.java
+++ src/main/java/ca/ece/ubc/cpen221/mp5/TestJSON.java
@@ -18,70 +18,51 @@ public class TestJSON {
 
 	
 	public static void main(String[] args) throws org.json.simple.parser.ParseException, FileNotFoundException, IOException {
-		/* JSONParser parser = new JSONParser();
 
-        try {     
-            Object obj = parser.parse(new FileReader("data/reviews.json"));
+		
+		String fileName = "C:/Users/Trainee/Desktop/CPEN221/f17-mp5-tylerlum_iadejumo/data/restaurants.json";
+		BufferedReader br =null;
+        JSONParser parser = new JSONParser();
 
-            JSONObject jsonObject =  (JSONObject) obj;
+        try {
 
-            String name = (String) jsonObject.get("name");
-            System.out.println(name);
+            String sCurrentLine;
 
-            String city = (String) jsonObject.get("city");
-            System.out.println(city);
+            br = new BufferedReader(new FileReader(fileName));
 
-            String job = (String) jsonObject.get("job");
-            System.out.println(job);
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println("Record:\t" + sCurrentLine);
 
-            // loop array
-            JSONArray cars = (JSONArray) jsonObject.get("cars");
-            Iterator<String> iterator = cars.iterator();
-            while (iterator.hasNext()) {
-             System.out.println(iterator.next());
+                Object obj;
+                try {
+                    obj = parser.parse(sCurrentLine);
+                    JSONObject jsonObject = (JSONObject) obj;
+
+                    String rel = (String) jsonObject.get("url");
+                    System.out.println(rel);
+
+                    Double start = (Double) jsonObject.get("longitude");
+                    System.out.println(start);
+
+                    Boolean end = (Boolean) jsonObject.get("open");
+                    System.out.println(end);
+
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+
         } catch (IOException e) {
             e.printStackTrace();
-        } 
-		*/
-		JSONParser parser = new JSONParser();
-		JSONArray a = (JSONArray) parser.parse(new FileReader("data/restaurants.json"));
-
-		  for (Object o : a)
-		  {
-		    JSONObject person = (JSONObject) o;
-
-		    String name = (String) person.get("name");
-		    System.out.println(name);
-
-		    String city = (String) person.get("city");
-		    System.out.println(city);
-
-		    String job = (String) person.get("job");
-		    System.out.println(job);
-
-		    JSONArray cars = (JSONArray) person.get("cars");
-
-		    for (Object c : cars)
-		    {
-		      System.out.println(c+"");
-		    }
-		  }
-		/*
-		List<Object> list = new ArrayList<Object>();
-		list.add("YO");
-		list.add(10);
-		list.add(new ArrayList<String>());
-		System.out.println(list);
-		list.add(1, 11);
-		System.out.println(list);
-		List<String> l = (List)(list.get(3));
-		l.add("e");
-		System.out.println(list);
-		*/
-		
+        } finally {
+            try {
+                if (br != null)br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+ 
     }
 	
 	public static void readJsonFile() {
